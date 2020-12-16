943308271a6702844997be9b9e2420378b5b0502
Ramon Bartl <rb@ridingbytes.com>
2020-12-16 10:36:57 +0100

    Updated build system to Webpack 5 (#47)
    * Updated build system for webpack 5
    
    * Rebuild Production JS
    
    * added webpack bundle analyzer
    
    * updated webpack config
    
    * rebuild production JS
    
    * Changelog updated
    
    * Pass environment variable for mode
    
    * Fixed issue in development mode and increased build performance
    
    * Remove styles for attachment_due (no longer required)
    
    * yarn build
    
    Co-authored-by: Jordi Puiggené <jp@naralabs.com>

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,11 @@ const path = require("path");
 const webpack = require("webpack");
 const childProcess = require("child_process");
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const gitCmd = "git rev-list -1 HEAD -- `pwd`";
@@ -10,16 +14,23 @@ let gitHash = childProcess.execSync(gitCmd).toString().substring(0, 7);
 
 const staticPath = path.resolve(__dirname, "../src/senaite/app/listing/static");
 
-const devMode = process.env.NODE_ENV !== 'production';
+const devMode = process.env.mode == "development";
+const prodMode = process.env.mode == "production";
+const mode = process.env.mode;
+console.log(`RUNNING WEBPACK IN '${mode}' MODE`);
 
 
 module.exports = {
+  // https://webpack.js.org/configuration/devtool
+  devtool: devMode ? "eval" : "source-map",
+  // https://webpack.js.org/configuration/mode/#usage
+  mode: mode,
   context: path.resolve(__dirname, "app"),
   entry: {
     listing: "./listing.coffee"
   },
   output: {
-    filename: gitHash ? `senaite.app.listing-${gitHash}.js` : "senaite.core.[name].js",
+    filename: devMode ? "senaite.app.[name].js" : `senaite.app.[name]-${gitHash}.js`,
     path: path.resolve(__dirname, "../src/senaite/app/listing/static/bundles"),
     publicPath: "++plone++senaite.app.listing.static/bundles"
   },
@@ -35,13 +46,53 @@ module.exports = {
         use: ["babel-loader"]
       }, {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   },
+  // https://webpack.js.org/configuration/optimization
+  optimization: {
+    minimize: prodMode,
+    minimizer: [
+      // https://webpack.js.org/plugins/terser-webpack-plugin/
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+          compress: {
+            drop_console: true,
+            passes: 2,
+          }
+	      },
+      }),
+      // https://webpack.js.org/plugins/css-minimizer-webpack-plugin/
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
+  },
   plugins: [
+    // https://webpack.js.org/plugins/mini-css-extract-plugin
+    new MiniCssExtractPlugin({
+      filename: devMode ? "senaite.app.[name].css" : `senaite.app.[name]-${gitHash}.css`,
+    }),
+    // https://github.com/webpack-contrib/webpack-bundle-analyzer
+    // new BundleAnalyzerPlugin(),
     // https://github.com/johnagan/clean-webpack-plugin
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({
+      verbose: false,
+      // Workaround in `watch` mode when trying to remove the `resources.pt` in the parent folder:
+      // Error: clean-webpack-plugin: Cannot delete files/folders outside the current working directory.
+      cleanAfterEveryBuildPatterns: ["!../*"],
+    }),
     // https://webpack.js.org/plugins/html-webpack-plugin/
     new HtmlWebpackPlugin({
       inject: false,
@@ -59,9 +110,6 @@ module.exports = {
     react: "React",
     "react-dom": "ReactDOM",
     $: "jQuery",
-    jquery: "jQuery",
-    jsi18n: {
-      root: "_t"
-    }
+    jquery: "jQuery"
   }
 };
