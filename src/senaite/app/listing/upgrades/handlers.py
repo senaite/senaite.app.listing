edb8f64a8513c1b74c2f743ce8506a12ec8c7fcd
Ramon Bartl <rb@ridingbytes.com>
2023-03-10 14:21:37 +0100

    Development is now 2.5.0

--- src/senaite/app/listing/upgrades/handlers.py
+++ src/senaite/app/listing/upgrades/handlers.py
@@ -23,6 +23,19 @@ from senaite.app.listing import logger
 PROFILE_ID = "profile-senaite.app.listing:default"
 
 
+def to_2500(portal_setup):
+    """Update to version 2.5.0
+
+    :param portal_setup: The portal_setup tool
+    """
+
+    logger.info("Run all import steps from SENAITE APP LISTING ...")
+    context = portal_setup._getImportContext(PROFILE_ID)
+    portal = context.getSite()  # noqa
+    portal_setup.runAllImportStepsFromProfile(PROFILE_ID)
+    logger.info("Run all import steps from SENAITE APP LISTING [DONE]")
+
+
 def to_2400(portal_setup):
     """Update to version 2.4.0
 
