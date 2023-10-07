a6f42ce6f992eb522674bc03010b3b30e57d505a
Ramon Bartl <rb@ridingbytes.com>
2023-10-07 13:11:11 +0200

    confirmation

--- webpack/app/components/ContextMenu.js
+++ webpack/app/components/ContextMenu.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { Menu, Item, Separator } from "react-contexify"
 import "react-contexify/dist/ReactContexify.css"
 import { CONFIRM_TRANSITION_IDS }from "./Constants.js"
@@ -7,10 +8,34 @@ const ContextMenu = function ContextMenu({...props}) {
 
   const callback = props.on_menu_item_click
 
+  useEffect(() => {
+    // componentDidUpdate
+  });
+
   function on_menu_item_click({event, props, triggerEvent, data}) {
     console.log(event, props, triggerEvent, data)
     if (callback) {
-      callback(data.id, data.url, props.item)
+      if (CONFIRM_TRANSITION_IDS.includes(data.id)) {
+        // add confirmation dialog on the menu item
+        let el = $(event.currentTarget)
+        const on_ok = () => callback(data.id, data.url, props.item)
+        el.confirmation({
+          rootSelector: "[data-toggle=confirmation]",
+          title: `${window._t(data.title)}?`,
+          btnOkLabel: window._t("Yes"),
+          btnOkClass: "btn btn-outline-primary",
+          btnOkIconClass: "fas fa-check-circle mr-1",
+          btnCancelLabel: window._t("No"),
+          btnCancelClass: "btn btn-outline-secondary",
+          btnCancelIconClass: "fas fa-circle mr-1",
+          container: "body",
+          onConfirm: on_ok,
+          singleton: true
+        })
+        el.confirmation("show")
+      } else {
+        callback(data.id, data.url, props.item)
+      }
     }
   }
 
@@ -21,10 +46,16 @@ const ContextMenu = function ContextMenu({...props}) {
     let folderitems = props.menu.folderitems || []
     let count = folderitems.length
 
+    // use the item title if we have only one (regular) folderitem
+    if (count == 1 && folderitems[0].title) {
+      title = folderitems[0].title
+    }
+
     // Title item
     menu_items = [
       <Item key="title" disabled>
-        <span className="badge badge-secondary mr-1">{count}</span> {title}
+        {count > 1 && <span className="badge badge-secondary mr-1">{count}</span>}
+        {title}
       </Item>,
       <Separator key="separator_title" />
     ]
@@ -33,8 +64,8 @@ const ContextMenu = function ContextMenu({...props}) {
     let transitions = props.menu.transitions || []
     for (let transition of transitions) {
       menu_items.push(
-        <Item key={transition.id} data={transition} onClick={on_menu_item_click}>
-          {transition.title}
+        <Item closeOnClick={false} key={transition.id} data={transition} onClick={on_menu_item_click}>
+          {window._t(transition.title)}
         </Item>
       )
     }
