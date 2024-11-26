30af625164f08b383d3f142cbd1d1e6b63d926d8
Jordi Puiggené <jp@naralabs.com>
2024-11-26 15:20:08 +0100

    Use chunkhash for static resources js and css in production mode

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const src_path = "../src/senaite/app/listing/browser/static"
+
 const path = require("path");
 const webpack = require("webpack");
 const childProcess = require("child_process");
@@ -9,10 +11,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const gitCmd = "git rev-list -1 HEAD -- `pwd`";
-let gitHash = childProcess.execSync(gitCmd).toString().substring(0, 7);
-
-const staticPath = path.resolve(__dirname, "../src/senaite/app/listing/browser/static");
+const staticPath = path.resolve(__dirname, src_path);
 
 const devMode = process.env.mode == "development";
 const prodMode = process.env.mode == "production";
@@ -28,9 +27,8 @@ module.exports = {
     listing: "./listing.coffee"
   },
   output: {
-    // filename: devMode ? "senaite.app.[name].js" : `senaite.app.[name]-${gitHash}.js`,
-    filename: "senaite.app.[name].js",
-    path: path.resolve(__dirname, "../src/senaite/app/listing/browser/static/bundles"),
+    filename: devMode ? "senaite.app.[name].js" : "senaite.app.[name].[chunkhash].js",
+    path: path.resolve(staticPath, "bundles"),
     publicPath: "++plone++senaite.app.listing.static/bundles"
   },
   module: {
@@ -85,8 +83,7 @@ module.exports = {
   plugins: [
     // https://webpack.js.org/plugins/mini-css-extract-plugin
     new MiniCssExtractPlugin({
-      // filename: devMode ? "senaite.app.[name].css" : `senaite.app.[name]-${gitHash}.css`,
-      filename: "senaite.app.[name].css",
+      filename: devMode ? "senaite.app.[name].css" : "senaite.app.[name].[chunkhash].css",
     }),
     // https://github.com/webpack-contrib/webpack-bundle-analyzer
     // new BundleAnalyzerPlugin(),
